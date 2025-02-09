dd58e6e3ab0f3658a6e33cdf10e9c2854c666e10
Kaslanas <victor24274@gmail.com>
2025-02-09 14:09:04 -0500

    Script
    Travail sur le C#

--- Assets/Script/Bill.cs
+++ Assets/Script/Bill.cs
@@ -1,18 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Bill : MonoBehaviour
 {
+
+    [SerializeField]public bool inWindZone = false;
+    public GameObject windZone;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         
+
+
+    }
+
+        void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Cible")){
+                Destroy(collision.gameObject);}
+        
+
+        
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+
+        if(coll.gameObject.CompareTag("SPEED"))
+        {
+            windZone = coll.gameObject;
+            inWindZone = true;
+        }
+
+        if(coll.gameObject.CompareTag("Finish"))
+        {
+            print("Game Finsih");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        }
+    }
+
+
+    void OnTriggerExit(Collider coll)
+    {
+        if(coll.gameObject.CompareTag("SPEED"))
+        {
+            inWindZone = false;
+
+        }
+    }
+
+    private void FixedUpdate(){
+
+     if(inWindZone){
+
+
+            rb.AddForce(windZone.GetComponent<ZonedeVitess>().direction * windZone.GetComponent<ZonedeVitess>().force);
+
+        }
+
     }
 }
--- Assets/Script/Levier.cs
+++ Assets/Script/Levier.cs
@@ -1,9 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Levier : MonoBehaviour
 {
+
+[SerializeField] public GameObject Obstacle;
+[SerializeField] public float Target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,18 @@ public class Levier : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float currentRotation = transform.rotation.eulerAngles.x;
+
+
+        if(currentRotation > Target){
+
+            Obstacle.SetActive(false);
+
+
+        }
+
+
         
     }
+
 }
